36fb847c03cb12871766a303801984302fde2140
Jean van Niekerk <jean.jacque@rocketmail.com>
2020-01-17 10:34:49 +0200

    Events and forms

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,32 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { User } from '../../models/User';
+import { User } from "../../models/User";
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  selector: "app-user",
+  templateUrl: "./user.component.html",
+  styleUrls: ["./user.component.css"]
 })
-
 export class UserComponent implements OnInit {
   // Properties
   user: User;
 
   // Methods
-  constructor() {
-    
-  } 
+  constructor() {}
 
   ngOnInit() {
     this.user = {
-      firstName: 'John',
-      lastName: 'Doe',
-      age: 30,
-      address: {
-        street: '50 Main st',
-        city: 'Boston',
-        state: 'MA'
-      }
-    }
+      firstName: "John",
+      lastName: "Doe",
+      email: ""
+    };
   }
 }
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,12 +1,8 @@
 export interface User {
-  firstName: string,
-  lastName: string,
-  age?: number,
-  address?: {
-    street?: string,
-    city?: string,
-    state?: string
-  },
-  isActive?: boolean,
-  registered?: any
+  firstName: string;
+  lastName: string;
+  email: string;
+  isActive?: boolean;
+  registered?: any;
+  hide?: boolean;
 }
